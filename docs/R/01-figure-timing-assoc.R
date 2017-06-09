d5e349606e7a110152587b4a9c6127afdc3db6cd
variani <machovariani@gmail.com>
2017-06-09 14:13:05 -0400

    created front fig

--- docs/R/01-figure-timing-assoc.R
+++ docs/R/01-figure-timing-assoc.R
@@ -1,10 +1,27 @@
 ### inc
 library(microbenchmark)
+library(ggplot2)
+
+theme_set(theme_minimal())
 
 ### simulate data
-N <- 1000
-M <- 2000
+N <- 1e3
+M <- 5e3
 
 simdat <- matlm_sim_randpred(seed = 1, N = N, M = M)
 
+### run benchmarks
+out <- microbenchmark(
+  "matlm" = matlm(simdat$form, simdat$dat, pred = simdat$pred),
+  "lm" = matlm_mod(simdat$form, simdat$dat, pred = simdat$pred),
+  times = 5)
+  
+### plot
+title <- paste(N, "individuals\n", M, "predictors")
 
+p <- autoplot(out) + labs(title = title) +
+  theme(text = element_text(size = 20))
+
+png("timing-matlm-vs-lm.png", width = 2*480, height = 480)
+print(p)
+dev.off()
