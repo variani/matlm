74c91dbe328b8fed07551c4391cddfd826bbe865
variani <machovariani@gmail.com>
2017-06-13 22:39:20 -0400

    vig

--- tests/testthat/test-sim.R
+++ tests/testthat/test-sim.R
@@ -2,11 +2,13 @@ context("sim")
 
 test_that("correlated predictors", {
   rho <- 0.9
+  N <- 1000
+  M <- 6
 
   C <- matrix(rho, M, M)
   diag(C) <- 1
 
-  simdat <- matlm_sim_randpred(seed = 1, N = 1000, M = 6, rho = rho)
+  simdat <- matlm_sim_randpred(seed = 1, N = N, M = M, rho = rho)
  
   C_data <- simdat$pred %>% cor %>% round(1)
   
