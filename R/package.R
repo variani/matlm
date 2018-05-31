2987b260626b3992cf85e95e0b972ad89844c9ca
variani <machovariani@gmail.com>
2018-05-31 12:08:09 -0400

    added exports; cleaned printed msg

--- R/package.R
+++ R/package.R
@@ -10,7 +10,7 @@
 #' @import bigmemory
 #' @import tictoc
 #' @importFrom tibble data_frame as_data_frame 
-#' @importFrom dplyr mutate select arrange everything bind_rows bind_cols
+#' @importFrom dplyr mutate select arrange everything bind_rows bind_cols full_join filter
 #' @importFrom magrittr %>%
 #' @importFrom stats lm model.extract model.frame model.matrix 
 #' @importFrom parallel makeCluster clusterExport parSapply stopCluster
